2a69d020f746310ae4e6cac7d9a415117557b4f7
Rania Widijanto <widijanto@shibe.local>
2020-09-18 20:01:46 +0700

    Did i add the correct files

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.helloworld.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
